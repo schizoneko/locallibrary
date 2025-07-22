96163afdbd342dbc8b654f0723a69b5ea4d49fc7
schizoneko <viettung11122004@gmail.com>
2025-07-22 20:39:57 +0700

    ADD part 6

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/')),
+    path("", views.index, name="index"),
+    path("books/", views.BookListView.as_view(), name="books"),
+    path("books/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
 ]

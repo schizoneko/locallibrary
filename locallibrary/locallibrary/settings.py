acda7564791616b8878adfa28632871cf581d830
schizoneko <viettung11122004@gmail.com>
2025-07-24 16:43:33 +0700

    ADD part 9

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -125,4 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
